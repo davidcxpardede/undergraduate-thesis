783a9187a477d9d65c205c6e596640815319f85f
David Pardede <davidpardede40@gmail.com>
2022-01-14 11:42:33 +0700

    Poker 3 (Randomly Distributed, but not unique)

--- poker.cpp
+++ poker.cpp
@@ -9,24 +9,32 @@ int main()
 {
     
     // Defining all cards in the deck based on suits and values  
-    string suits[4] = {"Diamonds", "Clubs", "Hearts", "Spades"};
     string values[13] = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-
+    string suits[4] = {"Diamonds", "Clubs", "Hearts", "Spades"};
+    
     // Declaring truly random variables to call from the array
     srand(time(0));
     int v1, s1;
-    string v2, s2;
+    int v2[5], s2[5];
 
-    for (int i = 0; i = 5; i++)
+    for (int i = 0; i < 5; i++)
     {
         v1 = rand() % 12;
         s1 = rand() % 3;
 
         v2[i] = v1;
         s2[i] = s1;
-    }    
 
-    cout << v2 << endl << s2;
+        /*for (j = i; j < )
+        values[j] = values[j-1];
+        suits[j] = suits[j-1]*/
+    }
+
+    cout << "The first card is the " << values[v2[0]] + " of " + suits[s2[0]] + "." << endl;
+    cout << "The second card is the " << values[v2[1]] + " of " + suits[s2[1]] + "." << endl;
+    cout << "The third card is the " << values[v2[2]] + " of " + suits[s2[2]] + "." << endl;
+    cout << "The fourth card is the " << values[v2[3]] + " of " + suits[s2[3]] + "." << endl;
+    cout << "The fifth card is the " << values[v2[4]] + " of " + suits[s2[4]] + "." << endl;
 
     return 0;
 }
