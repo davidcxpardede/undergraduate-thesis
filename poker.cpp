a0686e8c0eaad0bf5bf195c549a0372838b67de5
David Pardede <davidpardede40@gmail.com>
2022-01-14 10:12:38 +0700

    Poker 2

--- poker.cpp
+++ poker.cpp
@@ -8,28 +8,21 @@ using namespace std;
 int main()
 {
     
-    // Defining all cards in the deck based on suits and values
-    
+    // Defining all cards in the deck based on suits and values  
     string suits[4] = {"Diamonds", "Clubs", "Hearts", "Spades"};
     string values[13] = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
 
-    int a, b;
+    // Declaring truly random variables to call from the array
+    srand(time(0));
+    int a = rand() % 12;
+    int b = rand() % 3;
 
-    // Asking the user for input of the cards values and suits
-
-    cout << "What is the value of your card? ";
-    cin >> a;
-
-    cout << "What is the suit of your card? ";
-    cin >> b;
-
-    // Combining the values and suits into meaningful cards
-
+    // Defining the cards from the combination of random value and suit
     string cards = values[a] + " of " + suits[b];
 
-    // Displaying the cards
-
+    // Printing the cards
     cout << "Your card is the " << cards << "." << endl;
+
 
     return 0;
 }
