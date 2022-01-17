1b178bd09a1e1708d01105242443c4b74deac9c6
David Pardede <davidpardede40@gmail.com>
2022-01-17 11:53:33 +0700

    While Loop

---
+++ while-statement-fCC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int index = 6;
+
+    do {
+        cout << index << endl;
+        index +=1;
+    }   while(index <= 5);
+
+    return 0;
+}
