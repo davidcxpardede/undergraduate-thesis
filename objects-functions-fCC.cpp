6f99dbf4ab1105843fdd554d1d70562c83f40b24
David Pardede <davidpardede40@gmail.com>
2022-01-24 11:10:06 +0700

    Object Functions

--- objects-functions-fCC.cpp
+++ objects-functions-fCC.cpp
@@ -12,7 +12,12 @@ class Student {
             gpa = aGpa;
         }
 
-        bool hasHonors;
+        bool hasHonors () {
+            if(gpa >= 3.0) {
+                return true;
+            }
+            return false;
+        };
 
 };
 
@@ -20,6 +25,9 @@ int main()
 {
     Student student1("Jim", "Business", 2.4);
     Student student2("Pam", "Art", 3.6);
+    Student student3("David", "Engineering", 3.0);
+
+    cout << student3.hasHonors();
 
     return 0;
 }
