fac8003d1717ca0ff4df702af0a3e4d52fc8a0bf
davidcxpardede <davidpardede40@gmail.com>
2022-01-25 11:02:37 +0700

    Pointers 3

--- pointers/pointers-3.cpp
+++ pointers/pointers-3.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 // Pointer Types, Void Pointer, Pointer Arithmetic
-
+/*
+    1. A pointer type must be the same as the type of the variables it points to.
+    2. This is because pointers are used to dereference.
+    3. 
+*/
 int main()
 {
 
+
+    return 0;
 }
