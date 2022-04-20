a99cfc06767cced7f487cf2615cc445c9f93930c
davidcxpardede <davidpardede40@gmail.com>
2022-04-20 13:10:22 +0700

    Commit

---
+++ star_centroiding/imread.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+    string path = "./image.jpg";
+    Mat img = imread(path);
+
+
+}
