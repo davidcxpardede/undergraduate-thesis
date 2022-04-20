63db525f3fe986785581d92a42e9f0b511967cca
davidcxpardede <davidpardede40@gmail.com>
2022-04-20 10:07:23 +0700

    Commit

--- opencv_tutorial/main.cpp
+++ opencv_tutorial/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char** argv )
 {
 
-    Mat image;
+    cv::Mat image;
     image = imread("./picture.jpg");
     if ( !image.data )
     {
--- opencv_tutorial/murtaza/chapter1.cpp
+++ opencv_tutorial/murtaza/chapter1.cpp
@@ -3,5 +3,15 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
-using namespace cv;
 using namespace std;
+
+int main()
+{
+    string path = "Resources/test.png";
+    cv::Mat img = cv::imread(path);
+    cv::imshow("Image",img);
+    cv::waitKey(0);
+
+
+    return 0;
+}
